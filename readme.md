3efcc13d3e31bae7df28f847717ee92c3fb61703
sachincoder1 <sachinhlo232@gmail.com>
2022-05-30 14:02:54 +0530

    get balance and constructor

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Vending Machine Smart Contract
+
+
+# Things used in the project :-
+
+1. Solidity programming language.
+2. Ethereum.
+3. Remix IDE.
+
+
+
+
+# Architecture of the project :-
+
+**Global Variables** :- 
+1. Owner -> The real owner who deployed the contract ( type address ).
+2. Balances -> All the players address will be stored in this dynamic array ( type address[] ).
+
+
+**Functions**  :- 
+1. purchase
+2. reStock
+3. get balance.
--- vendingMachine.sol
+++ vendingMachine.sol
@@ -1,3 +1,27 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
+
+contract VendingMaching {
+    address public owner; // owner of this contract;
+    mapping(address => uint) public canBalances; // This vending machine sells cans;
+
+    // Constructor
+    constructor(){
+        owner = msg.sender;
+        canBalances[address(this)] = 100;
+    }
+
+
+    // get balance of vending machine.
+    function vendingBalance() public view returns(uint) {
+        return canBalances[address(this)];
+    }
+
+
+    
+
+
+
+
+}

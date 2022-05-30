2e5f245e7eac0c854624121faf5ad0db22effa00
sachincoder1 <sachinhlo232@gmail.com>
2022-05-30 15:21:41 +0530

    purchase and restock.

--- vendingMachine.sol
+++ vendingMachine.sol
@@ -12,6 +12,13 @@ contract VendingMaching {
         canBalances[address(this)] = 100;
     }
 
+    // modifier for only owner;
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+
 
     // get balance of vending machine.
     function vendingBalance() public view returns(uint) {
@@ -19,7 +26,25 @@ contract VendingMaching {
     }
 
 
-    
+    // restock the vending machine, only owner can do this. (if all cans are over then refill the vending machine with cans.);
+    function reStock(uint amount) public onlyOwner {
+       canBalances[address(this)] += amount;
+    }
+
+
+    // purchase can; 
+    function purchaseCan(uint _amount) public payable {
+        require(msg.value >= _amount * 2 ether, "The price of one can is 2 ether");
+        require(canBalances[address(this)] >= _amount, "Sorry, Not enough cans available. Try again later or try to get less quantity..");
+        canBalances[address(this)] -= _amount;
+        canBalances[msg.sender] += _amount;
+
+    }
+
+
+
+
+
 
 
 
